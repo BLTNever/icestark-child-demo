469d83e578ecc63030b70c49e334e524a40d51ef
Zhang Alec <zhangalec@ZhangdeMacBook-Pro.local>
2020-04-09 11:20:49 +0800

    update

---
+++ precache-manifest.173bd9cc4937758fe54f8a3b78e3ccff.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0bc202044bba7ecb29b30804fd8a8d5",
+    "url": "//dev-goods.ypsx-internal.com/index.html"
+  },
+  {
+    "revision": "cba285a3a3a5b72be86f",
+    "url": "//dev-goods.ypsx-internal.com/static/css/2.59251d41.chunk.css"
+  },
+  {
+    "revision": "570d53e2de4a25b2a0ea",
+    "url": "//dev-goods.ypsx-internal.com/static/css/main.cdcb1853.chunk.css"
+  },
+  {
+    "revision": "cba285a3a3a5b72be86f",
+    "url": "//dev-goods.ypsx-internal.com/static/js/2.1973e46e.chunk.js"
+  },
+  {
+    "revision": "148e91f08cc6aec66b4d",
+    "url": "//dev-goods.ypsx-internal.com/static/js/3.37c046ea.chunk.js"
+  },
+  {
+    "revision": "7cfc55b9a645444dcb93",
+    "url": "//dev-goods.ypsx-internal.com/static/js/4.9483b10a.chunk.js"
+  },
+  {
+    "revision": "570d53e2de4a25b2a0ea",
+    "url": "//dev-goods.ypsx-internal.com/static/js/main.83a6fbb8.chunk.js"
+  },
+  {
+    "revision": "5f9183a6df22f1982e50",
+    "url": "//dev-goods.ypsx-internal.com/static/js/runtime~main.56550404.js"
+  }
+]);
