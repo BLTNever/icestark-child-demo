c857ef53efed421f0c0157d139f8d354299afacc
Zhang Alec <zhangalec@ZhangdeMacBook-Pro.local>
2020-04-09 16:04:59 +0800

    ipdate

---
+++ precache-manifest.5c44476c7bf5b06f413852b0d3659450.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d736ebe398b4bd230ab579b29cc117ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2fd73be1b626b639f6c3",
+    "url": "/static/css/2.59251d41.chunk.css"
+  },
+  {
+    "revision": "570d53e2de4a25b2a0ea",
+    "url": "/static/css/main.cdcb1853.chunk.css"
+  },
+  {
+    "revision": "2fd73be1b626b639f6c3",
+    "url": "/static/js/2.74737fc0.chunk.js"
+  },
+  {
+    "revision": "148e91f08cc6aec66b4d",
+    "url": "/static/js/3.37c046ea.chunk.js"
+  },
+  {
+    "revision": "7cfc55b9a645444dcb93",
+    "url": "/static/js/4.9483b10a.chunk.js"
+  },
+  {
+    "revision": "570d53e2de4a25b2a0ea",
+    "url": "/static/js/main.83a6fbb8.chunk.js"
+  },
+  {
+    "revision": "adc2ffd0dd0f3c8d3630",
+    "url": "/static/js/runtime~main.7b3e5a33.js"
+  }
+]);
