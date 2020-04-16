3579f17b7b0ecfd3981ca2b933b7317934549b9b
Zhang Alec <zhangalec@ZhangdeMacBook-Pro.local>
2020-04-16 19:01:25 +0800

    update

---
+++ precache-manifest.671e11bd085f8d99e8a3cca6d3186fd1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0d3f104b7d0b630935cb7480f82e2e0",
+    "url": "//louisedylan.github.io/icestrak-child-demo/index.html"
+  },
+  {
+    "revision": "075a4a6b6ce6dd8db9c2",
+    "url": "//louisedylan.github.io/icestrak-child-demo/static/css/2.59251d41.chunk.css"
+  },
+  {
+    "revision": "a726ee373b9970a6ee79",
+    "url": "//louisedylan.github.io/icestrak-child-demo/static/css/main.cdcb1853.chunk.css"
+  },
+  {
+    "revision": "075a4a6b6ce6dd8db9c2",
+    "url": "//louisedylan.github.io/icestrak-child-demo/static/js/2.fe276404.chunk.js"
+  },
+  {
+    "revision": "148e91f08cc6aec66b4d",
+    "url": "//louisedylan.github.io/icestrak-child-demo/static/js/3.37c046ea.chunk.js"
+  },
+  {
+    "revision": "7cfc55b9a645444dcb93",
+    "url": "//louisedylan.github.io/icestrak-child-demo/static/js/4.9483b10a.chunk.js"
+  },
+  {
+    "revision": "a726ee373b9970a6ee79",
+    "url": "//louisedylan.github.io/icestrak-child-demo/static/js/main.a0632ea9.chunk.js"
+  },
+  {
+    "revision": "b424632a64513fcf0ae9",
+    "url": "//louisedylan.github.io/icestrak-child-demo/static/js/runtime~main.1411fff8.js"
+  }
+]);
